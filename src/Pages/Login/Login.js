36440d53a8b4d9edc7eefb66f392d6fa6bc3e639
noman081 <abdullahalnoman642@gmail.com>
2022-04-13 06:01:26 +0600

    After completing email pass auth

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,28 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import logo from '../../images/google_logo.png';
 const Login = () => {
-    return (
-        <form className='contianer w-50 mx-auto mt-5 border border-secondary p-3 rounded-3 shadow-lg'>
-            <h1 className='text-center text-primary'>Please Login</h1>
-            <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-                <label for="floatingInput">Email address</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-                <label for="floatingPassword">Password</label>
-            </div>
-            <button type="button" className="btn btn-outline-dark ms-0">Login</button>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log('submitted', email, password);
+        signInWithEmailAndPassword(email, password);
 
+    }
+    if (user) {
+        navigate('/');
+    }
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+    return (
+        <form onSubmit={handleSubmit} className='contianer w-50 mx-auto mt-5 p-3 rounded-3 shadow-lg'>
+            <h1 className='text-center text-primary'>Please Login</h1>
+            <div className="form-floating mb-3">
+                <input onBlur={handleEmailBlur} type="email" className="form-control" id="floatingInput" placeholder="name@example.com" required />
+                <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating mb-3">
+                <input onBlur={handlePasswordBlur} type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <input type="submit" className="btn btn-outline-dark ms-0" value="Login" />
+            {
+                error && <p className="text-danger">{error}</p>
+            }
             <div className='text-center mt-3'>
                 <h4>or</h4>
                 <hr />
-                <button type="button" class="btn btn-outline-dark"><img height='40px' width='100%' src={logo} alt="" /></button>
+                <button type="button" className="btn btn-outline-dark"><img height='40px' width='100%' src={logo} alt="" /></button>
             </div>
             <div className='text-center mt-3'>
                 <p>
-                    New to Ema-John? <Link className='text-primary' to="/signup">Create an account</Link>
+                    New to <span className='text-warning text-bold'>Best Tour Plan?</span> <Link className='text-primary' to="/signup">Create an account</Link>
                 </p>
             </div>
         </form>
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,29 +1,76 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../images/google_logo.png';
-
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [customError, setCustomError] = useState('');
+    const navigate = useNavigate();
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log('submitted', email, password, confirmPassword);
+        if (password !== confirmPassword) {
+            setCustomError('Password should match!');
+            return;
+        }
+        if (password.length < 6) {
+            setCustomError('Password should longer than 6 characters!');
+            return;
+        }
+        createUserWithEmailAndPassword(email, password);
+    }
+    if (user) {
+        navigate('/');
+    }
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+    const handleConfirmPasswordBlur = event => {
+        setConfirmPassword(event.target.value);
+    }
     return (
-        <form className='contianer w-50 mx-auto mt-5 border border-secondary p-3 rounded-3'>
+        <form onSubmit={handleSubmit} className='contianer w-50 mx-auto mt-5 border border-secondary p-3 rounded-3'>
             <h1 className='text-center text-primary'>Please SignUp</h1>
-            <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-                <label for="floatingInput">Email address</label>
+            <div className="form-floating mb-3">
+                <input onBlur={handleEmailBlur} type="email" className="form-control" id="floatingInput" placeholder="name@example.com" required />
+                <label htmlFor="floatingInput">Email address</label>
             </div>
-            <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-                <label for="floatingPassword">Password</label>
+            <div className="form-floating mb-3">
+                <input onBlur={handlePasswordBlur} type="password" className="form-control" id="floatingPassword" placeholder="Password" required />
+                <label htmlFor="floatingPassword">Password</label>
             </div>
-            <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Confirm Password" />
-                <label for="floatingPassword">Confirm Password</label>
+            <div className="form-floating mb-3">
+                <input onBlur={handleConfirmPasswordBlur} type="password" className="form-control" id="floatingPassword" placeholder="Confirm Password" required />
+                <label htmlFor="floatingPassword">Confirm Password</label>
             </div>
-            <button type="button" className="btn btn-outline-dark ms-0">Sign Up</button>
 
+            {
+                customError && <p className='text-danger'>{customError}</p>
+            }
+
+            <button type="submit" className="btn btn-outline-dark ms-0">Sign Up</button>
+            {
+                error && <p className='text-danger'>{error}</p>
+            }
+            {
+                loading && <p className='text-success'>Loading............</p>
+            }
             <div className='text-center mt-3'>
                 <h4>or</h4>
                 <hr />
-                <button type="button" class="btn btn-outline-dark"><img height='40px' width='100%' src={logo} alt="" /></button>
+                <button type="button" className="btn btn-outline-dark"><img height='40px' width='100%' src={logo} alt="" /></button>
             </div>
             <div className='text-center mt-3'>
                 <p>
